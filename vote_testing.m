52fc1ab41257d67f26241fffdb33307211844666
Jonathan Taylor <jhhtaylor@gmail.com>
2021-09-28 19:14:07 +0200

    Create vote_testing.m

---
+++ vote_testing.m
@@ -0,0 +1,12 @@
+clc;
+%note: in this toy example, the only valid codewords are '0000000' and
+%'1111111'
+H = [1 1 0 0 0 0 0;
+    0 1 1 0 0 0 0;
+    0 1 1 1 1 0 0;
+    0 0 0 1 1 0 0;
+    0 0 0 0 1 1 0;
+    0 0 0 0 1 0 1]
+
+msg = [0 0 0 0 0 0 0]
+rcvd_msg = [0 1 0 0 1 0 0]
