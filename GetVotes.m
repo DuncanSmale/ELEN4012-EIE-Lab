f00a7d14db3161d4ceef108d717eb45a31bccb26
Jonathan Taylor <jhhtaylor@gmail.com>
2021-09-29 12:07:36 +0200

    More generalized voting
    Added my voting algorithm to GetVotes.m

--- GetVotes.m
+++ GetVotes.m
@@ -2,17 +2,36 @@ function votes = GetVotes(H,codeword)
 % H:        parity check matrix
 % codeword: received codeword
 % votes:    votes for each node
-votes = zeros(size(codeword));
 
-% loop over each row and column
-% find what each parity check node requires from the matrix
-% check if each codeword satisfies the parity check node
-% if it doesnt 
-[n,m] = size(H);
-for i = 1:n
-    for j = 1:m
-        
+[rownum,colnum]=size(H);
+votes = zeros(1,colnum); %this is the final output variable
+parity_store = zeros(1,rownum);
+
+%% First we collect the parity checks - pass (0) or fail (1)?
+for rowindex = 1:rownum
+    parity_tracker = 0;%reset every row
+    for colindex = 1:colnum % we are in a specific row, going through columns
+        %z = H(rowindex,colindex) prints along column and then row in a Z pattern
+        if H(rowindex,colindex) == 1
+            %do the corresponding message bits have an even parity?
+            if codeword(colindex) == 1
+                %disp("One")
+                parity_tracker = xor(parity_tracker,codeword(colindex));
+            else
+                %disp("Zero")
+            end
+        end
+    end
+    %parity_tracker % 1 = fail, 0 = pass
+    parity_store(rowindex) = parity_tracker;
+end
+%% Second we allocate which bits have caused those parity checks to fail (i.e. the votes)
+for rowindex = 1:rownum
+    for colindex = 1:colnum
+        if H(rowindex,colindex) == 1
+            votes(colindex) = votes(colindex) + parity_store(rowindex);
+        end
     end
 end
-end
+%votes % the desired output
 
