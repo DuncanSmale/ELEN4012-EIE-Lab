45bf1a9a0c8b849ac4daf53332113ddebba7fd0c
DuncanSmale <duncan.smale@gmail.com>
2021-09-29 15:03:59 +0200

    Update generateData.m

--- generateData.m
+++ generateData.m
@@ -8,10 +8,10 @@ load H.mat H_rev
 type = InputTypes.LLR; 
 
 % how many messages/codewords to generate
-num_messages = 2000000;
+num_messages = 1000000;
 
 % make this between 0 and 1
-percent_noisy = 0.75;
+percent_noisy = 0.60;
 
 % rng seed to use
 seed = 1;
@@ -19,16 +19,32 @@ seed = 1;
 % Try make the total elements divisible by 10
 % range of SNR values to use in the noisy portion of the dataset
 % SNR = 0:0.2:4.8; 
-SNR = 0;
+SNR = -1;
 
 [dataset, messages] = CreateDataset(type, num_messages, H_rev, percent_noisy, seed, SNR);
+
+% dont change
+full_percent = num2str(percent_noisy * 100, '%u');
+datapoints = NumToString(num_messages);
 
 % change to desired name of text files
 % this function creates the folder of name LLR, change this to the string
 % version of string(type) i.e if its votes change LLR to votes
-% mkdir LLR 
-message_name = string(type) + "/2M75messages" + string(type) + ".txt";
-dataset_name = string(type) + "/2M75data" + string(type) + ".txt";
+mkdir(string(type))
+message_name = string(type) + "/" + datapoints + full_percent + "messages" + string(type) + ".txt";
+dataset_name = string(type) + "/" + datapoints + full_percent + "data" + string(type) + ".txt";
 
 writematrix(dataset, dataset_name);
 writematrix(messages, message_name);
+
+function num_string = NumToString(num)
+if mod(num, 1e6) ~= num
+    format = "%u";
+    if mod(num, 1e6) ~= 0
+        format = "%.1f";
+    end
+    num_string = num2str(num/1e6, format) + "M";
+elseif mod(num, 1e3) ~= num
+    num_string = num2str(num/1e3, "%u") + "K";
+end
+end
