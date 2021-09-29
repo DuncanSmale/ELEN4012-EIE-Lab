6351e08d48f826a37ae4a72aa2d4060694dd7544
Jonathan Taylor <jhhtaylor@gmail.com>
2021-09-29 11:26:10 +0200

    Very basic voting example working

--- vote_testing.m
+++ vote_testing.m
@@ -6,7 +6,52 @@ H = [1 1 0 0 0 0 0;
     0 1 1 1 1 0 0;
     0 0 0 1 1 0 0;
     0 0 0 0 1 1 0;
-    0 0 0 0 1 0 1]
+    0 0 0 0 1 0 1];
+[rownum,colnum]=size(H);
 
-msg = [0 0 0 0 0 0 0]
-rcvd_msg = [0 1 0 0 1 0 0]
+msg = [0 0 0 0 0 0 0];
+rcvd_msg = [0 1 0 0 1 0 0];
+votes = zeros(1,colnum); %this is the final output
+parity_store = zeros(1,rownum);
+% print columns
+
+% for j = 1:colnum
+%     H(:,j)
+% end
+
+%% First we collect the parity checks - pass (0) or fail (1)?
+for rowindex = 1:rownum
+    parity_tracker = 0;%reset every row
+    for colindex = 1:colnum % we are in a specific row, going through columns
+        %z = H(rowindex,colindex) prints along column and then row in a Z pattern
+        if H(rowindex,colindex) == 1
+            %do the corresponding message bits have an even parity?
+            if rcvd_msg(colindex) == 1
+                %disp("yooo")
+                parity_tracker = xor(parity_tracker,rcvd_msg(colindex));
+                %votes(colindex) = votes(colindex) + 1;
+            else
+                %disp("naaa")
+            end
+        end
+    end
+    %just a print
+    %parity_tracker % 1 = fail, 0 = pass
+    parity_store(rowindex) = parity_tracker;
+    %need another loop to add to votes
+    
+    %votes(rowindex) = votes(rowindex) + parity_tracker
+    %H(i,:) %prints rows
+end
+
+%parity_store
+%% Second we allocate which bits have caused those parity checks to fail (i.e. the votes)
+for rowindex = 1:rownum
+    for colindex = 1:colnum
+        if H(rowindex,colindex) == 1
+            votes(colindex) = votes(colindex) + parity_store(rowindex);
+        end
+    end
+end
+votes % the desired output
+
