d8f734910fa3457d4c6a4c457208402b8e3c8187
DuncanSmale <duncan.smale@gmail.com>
2021-09-30 17:35:41 +0200

    Update GetLLR.m

--- GetLLR.m
+++ GetLLR.m
@@ -3,9 +3,8 @@ function LLR = GetLLR(received, SNR)
 % SNR:          Signal to noise ration of the channel
 % LLR:          reliability of each node as an LLR (log likelihood ratio)
 SNR_dec = 10^(SNR/10);
-% N0_uncoded = 1/SNR_dec; % uncoded 
-% N0 = N0_uncoded/(1/2); % half rate code
-N0 = SNR_dec;
+N0_uncoded = 1/SNR_dec; % uncoded 
+N0 = N0_uncoded/(1/2); % half rate code
 LLR = (2/N0)*received;
 end
 
