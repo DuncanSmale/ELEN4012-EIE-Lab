415222b0b322471a469de170aa4f5f91a7e75183
DuncanSmale <duncan.smale@gmail.com>
2021-10-01 17:15:13 +0200

    Update generateData.m

--- generateData.m
+++ generateData.m
@@ -1,17 +1,17 @@
 %%% This script is used to generate the required data sets for training %%%
 %%% Change the parameters to alter the dataset given back %%%
-
+% TODO add SNR to all data sets and fix the size of the inputs to 201
 clc
 load H.mat H_rev
 
-% InputTypes are: Naive, LLR, Vote, LLRVote
+% InputTypes are: Naive, NaiveSyndrome, LLR, Vote, LLRVote
 type = InputTypes.LLR; 
 
 % how many messages/codewords to generate
-num_messages = 1000000;
+num_messages = 20000;
 
 % make this between 0 and 1
-percent_noisy = 0.60;
+percent_noisy = 0.9;
 
 % rng seed to use
 seed = 1;
@@ -19,20 +19,21 @@ seed = 1;
 % Try make the total elements divisible by 10
 % range of SNR values to use in the noisy portion of the dataset
 % SNR = 0:0.2:4.8; 
-SNR = -1;
+SNR = 0:2:6;
 
 [dataset, messages] = CreateDataset(type, num_messages, H_rev, percent_noisy, seed, SNR);
 
 % dont change
 full_percent = num2str(percent_noisy * 100, '%u');
 datapoints = NumToString(num_messages);
+numSNR = "_" + num2str(SNR(1)) + "_" + num2str(SNR(end)) + "SNR";
 
 % change to desired name of text files
 % this function creates the folder of name LLR, change this to the string
 % version of string(type) i.e if its votes change LLR to votes
 mkdir(string(type))
-message_name = string(type) + "/" + datapoints + full_percent + "messages" + string(type) + ".txt";
-dataset_name = string(type) + "/" + datapoints + full_percent + "data" + string(type) + ".txt";
+message_name = string(type) + "/" + datapoints + numSNR + full_percent + "messages" + string(type) + ".txt";
+dataset_name = string(type) + "/" + datapoints + numSNR + full_percent + "data" + string(type) + ".txt";
 
 writematrix(dataset, dataset_name);
 writematrix(messages, message_name);
