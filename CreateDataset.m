d6c575eb161dabe1f9655aa589a2278de8bd234f
Jonathan Taylor <jhhtaylor@gmail.com>
2021-10-07 20:20:34 +0200

    Added many input schemes to Schemes.m

--- CreateDataset.m
+++ CreateDataset.m
@@ -129,7 +129,7 @@ elseif type == InputTypes.NaiveMultVote %Quantized Naive * (Max Vote - Vote)
     temp_votes = zeros(size(dataset));
     %demodulate noisy part
     for j = 1:size(x,2)
-        x(:, j) = decode_demod_bpsk(real(demodulator(x(:, j)))',0); %Note this line is now decoded
+        x(:, j) = Schemes.decode_demod_bpsk(real(demodulator(x(:, j)))',0); %Note this line is now decoded
     end
 
     if percent_noisy ~= 0
@@ -139,10 +139,11 @@ elseif type == InputTypes.NaiveMultVote %Quantized Naive * (Max Vote - Vote)
         for j = 1:size(dataset,2)
             temp_votes(:,j) = GetVotes(H,dataset(:,j));
         end
-        max_votes = max(max(temp_votes));
+        %max_votes = max(max(temp_votes));
         %Add the votes to the dataset
         %dataset = dataset .* (1./(1+temp_votes));
-        dataset = dataset .* (max_votes-temp_votes);
+        dataset = Schemes.processNaiveMultVote(dataset,temp_votes);
+        %%%dataset = dataset .* (max_votes-temp_votes);
         %Replace dataset with only votes 
         %dataset = dataset .* (1./(1+temp_votes));
     end
@@ -240,7 +241,7 @@ elseif contains(string(type),string(InputTypes.LLRMultVote))
     if(contains(string(type),string(InputTypes.LLRMultVoteMultNaive)))
         x_naive = x_votes;
         for j = 1:size(x_naive,2)
-           x_naive(:, j) = decode_demod_bpsk(real(demodulator(x_naive(:, j)))',1);
+           x_naive(:, j) = Schemes.decode_demod_bpsk(real(demodulator(x_naive(:, j)))',1);
         end
     end
     %------------------------------
@@ -251,7 +252,7 @@ elseif contains(string(type),string(InputTypes.LLRMultVote))
         for j = 1:size(dataset_votes,2)
             temp_votes(:,j) = GetVotes(H,dataset_votes(:,j));
         end
-        max_votes = max(max(temp_votes));
+        %max_votes = max(max(temp_votes));
         %Other possibilities for input schemes
         %Add the votes to the dataset
         %dataset = dataset .* (1./(1+temp_votes));
@@ -261,9 +262,11 @@ elseif contains(string(type),string(InputTypes.LLRMultVote))
         %dataset = [x_votes ; dataset .* (max_votes-temp_votes)];
         
         if(contains(string(type),string(InputTypes.LLRMultVoteMultNaive)))
-            dataset = dataset .* (max_votes-temp_votes) .* x_naive;
+            %dataset = dataset .* (max_votes-temp_votes) .* x_naive;
+            dataset = Schemes.processLLRMultVoteMultNaive(x_naive,dataset,temp_votes);
         else
-            dataset = dataset .* (max_votes-temp_votes);
+            %dataset = dataset .* (max_votes-temp_votes);
+            dataset = Schemes.processLLRMultVote(dataset,temp_votes);
         end
         %[rowds,colds] = size(dataset)
     end
@@ -295,7 +298,7 @@ elseif type == InputTypes.LLRVoteRange
             actualSNR = snr(c_mod(:, k - index + 1), x_LLR(:, k)-c_mod(:, k - index + 1));
             x_LLR(:, k) = GetLLR(x_LLR(:, k), actualSNR);
             vec = x_thresh(:, k);
-            vec = FlipFromVote(vec, temp_votes(:,k), x_LLR(:, k));
+            vec = Schemes.processFlipFromVote(vec, x_LLR(:, k), temp_votes(:,k));
             dataset(:, k) = vec;
         end
         
---
+++ Schemes.m
@@ -0,0 +1,68 @@
+%This class holds novel input schemes and other useful methods
+classdef Schemes
+    methods(Static)
+        function datasetOut = processNaiveMultVote(NaiveValues,VoteValues)
+            max_votes = max(max(VoteValues));
+            datasetOut = (max_votes-VoteValues) .* NaiveValues;
+        end
+
+        function datasetOut = processLLRMultVoteMultNaive(NaiveValues,LLRValues,VoteValues)%dataset,tempvotes,x_naive)
+            max_votes = max(max(VoteValues));
+            datasetOut = LLRValues .* (max_votes-VoteValues) .* NaiveValues;
+        end
+
+        function datasetOut = processLLRMultVote(VoteValues,LLRValues)
+            max_votes = max(max(VoteValues));
+            datasetOut = LLRValues .* (max_votes-VoteValues);
+        end
+
+        function datasetOut = processFlipFromVote(NaiveValues, LLRValues, VoteValues)
+            threshold = 0.1;
+            datasetOut = LLRValues;
+            ind_LLR =  find(abs(NaiveValues)<threshold);
+            ind_votes = find(VoteValues>2);
+            lia = ismember(ind_LLR, ind_votes);
+            ind = ind_LLR .* lia;
+            vals = ind(ind~=0);
+            datasetOut(vals) = -datasetOut(vals);
+        end
+
+        %decodeAndDecode must be either true (1) or false (0) only.
+        function out = decode_demod_bpsk(rcvd, decodeAndDecode)
+            out = zeros(size(rcvd));
+            %Decodes and Demods - i.e. converts -9,872 to -1 to 1
+            if decodeAndDecode == true
+                for bit = 1:size(rcvd,2)
+                    if rcvd(bit) <= 0
+                        out(bit) = 1;
+                    else
+                        out(bit) = 0;
+                    end
+                end
+            %Only decodes - i.e. converts -9.872 to -1
+            elseif decodeAndDecode == false
+                for bit = 1:size(rcvd,2)
+                    if rcvd(bit) <= 0
+                        out(bit) = -1;
+                    else
+                        out(bit) = 1;
+                    end
+                end
+            else 
+                disp("decodeAndDecode must be either true (1) or false (0) only.");
+            end
+        end
+        
+    end
+end
+
+% function out = FlipFromVote(received, votes, LLR)
+% threshold = 0.1;
+% out = LLR;
+% ind_LLR =  find(abs(received)<threshold);
+% ind_votes = find(votes>2);
+% lia = ismember(ind_LLR, ind_votes);
+% ind = ind_LLR .* lia;
+% vals = ind(ind~=0);
+% out(vals) = -out(vals);
+% end
