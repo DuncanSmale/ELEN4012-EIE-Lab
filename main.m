adb8421f4f2c2953c755730992b3cfb2039ec73b
DuncanSmale <duncan.smale@gmail.com>
2021-10-08 09:20:10 +0200

    Update main.m

--- main.m
+++ main.m
@@ -3,14 +3,16 @@ clc
 % this is the file used to test some of the generation used for the lab
 
 file_prefix = "Python/models/";
-models = ["LLRVoteRange10K_0_6SNR100H2tanh.h5"];
+file_suffix = "10K_0_6SNR100H3tanh.h5";
+models = ["Naive", "LLR", "Vote", "NaiveMultVote",...
+    "LLRMultVote", "LLRMultVoteMultNaive", "LLRVoteRange"];
 labels = [];
 nets = cell(numel(models),1);
 for i = 1:numel(models)
-    file = file_prefix + models(i);
+    file = file_prefix + models(i) + file_suffix;
     nets{i} = importKerasNetwork(file);
 end
-SNR = -2:1:10;
+SNR = [0];
 
 load H.mat H_rev
 H = H_rev;
@@ -32,7 +34,7 @@ A_inv = inv(A_gf);
 I = [row col];
 index = sparse(I(:,1),I(:,2),1);
 decoderLDPC = comm.LDPCDecoder('ParityCheckMatrix',index);
-n_blocks = 1000;
+n_blocks = 1;
 decoder = zeros(size(SNR));
 keras = zeros(size(SNR,1), numel(nets));
 hard = zeros(size(SNR));
@@ -53,55 +55,56 @@ for  i = 1:numel(SNR)
         decoded_matlab = decoderLDPC(x);
         for k = 1:numel(nets)
             % getting relevant dataset
-            if contains(model(k), string(InputTypes.LLRVoteRange))
+            models(k)
+            if contains(models(k), string(InputTypes.LLRVoteRange))
                 %note I made the order now (Naive,LLR,Votes) for all novel input schemes in Schemes.m
-                flipped = Schemes.FlipFromVote(x_thresh,x, votes);
+                flipped = Schemes.processFlipFromVote(x_thresh,x, votes)
                 arr = [flipped; snr_calc]';
                 labels = [labels, string(InputTypes.LLRVoteRange)];
                 
-            elseif contains(model(k), string(InputTypes.LLRMultVoteMultNaive))
+            elseif contains(models(k), string(InputTypes.LLRMultVoteMultNaive))
                 %Also: Am I understanding 'Schemes.interpret_demod_bpsk(...)'
                 %correctly?
                 %My idea:
-                values = Schemes.processLLRMultVoteMultNaive(Schemes.interpret_demod_bpsk(x_thresh,0),x,votes)
-                arr = [values; snr_calc]';
+                LLRMultVoteMultNaive = Schemes.processLLRMultVoteMultNaive(Schemes.interpret_demod_bpsk(x_thresh,0),x,votes')
+                arr = [LLRMultVoteMultNaive; snr_calc]';
                 labels = [labels, string(InputTypes.LLRMultVoteMultNaive)];
-            elseif contains(model(k), string(InputTypes.LLRMultVote))
+            elseif contains(models(k), string(InputTypes.LLRMultVote))
                 %My idea:
-                values = Schemes.processLLRMultVote(x,votes)
-                arr = [values; snr_calc]';
+                LLRMultVote = Schemes.processLLRMultVote(x,votes')
+                arr = [LLRMultVote; snr_calc]';
                 labels = [labels, string(InputTypes.LLRMultVote)];
                 
-            elseif contains(model(k), string(InputTypes.NaiveMultVote))
+            elseif contains(models(k), string(InputTypes.NaiveMultVote))
                 %My idea:
-                values = Schemes.processNaiveMultVote(Schemes.interpret_demod_bpsk(x_thresh,0),votes)
-                arr = [values; snr_calc]';
+                NaiveMultVote = Schemes.processNaiveMultVote(Schemes.interpret_demod_bpsk(x_thresh',0),votes)
+                arr = [NaiveMultVote, snr_calc];
                 labels = [labels, string(InputTypes.NaiveMultVote)];
                 
-            elseif contains(model(k), string(InputTypes.LLRVote))
+            elseif contains(models(k), string(InputTypes.LLRVote))
                 %My idea:
-                values = [x, votes];
-                arr = [values;snr_calc]';
+                LLRVote = [x, votes]
+                arr = [LLRVote;snr_calc]';
                 labels = [labels, string(InputTypes.LLRVote)];
                 
-            elseif contains(model(k), string(InputTypes.NaiveVote))
+            elseif contains(models(k), string(InputTypes.NaiveVote))
                 %My idea:
-                values = [Schemes.interpret_demod_bpsk(x_thresh,0), votes];
-                arr = [values;snr_calc]';
+                NaiveVote = [Schemes.interpret_demod_bpsk(x_thresh,0), votes]
+                arr = [NaiveVote;snr_calc]';
                 labels = [labels, string(InputTypes.NaiveVote)];
                 
-            elseif contains(model(k), string(InputTypes.Vote))
+            elseif contains(models(k), string(InputTypes.Vote))
                 %My idea:
-                arr = [votes;snr_calc]';
+                arr = [votes,snr_calc];
                 labels = [labels, string(InputTypes.Vote)];
                 
-            elseif contains(model(k), string(InputTypes.LLR))
+            elseif contains(models(k), string(InputTypes.LLR))
                 arr = [x; snr_calc]';
                 labels = [labels, string(InputTypes.LLR)];
                 
-            elseif contains(model(k), string(InputTypes.Naive))
+            elseif contains(models(k), string(InputTypes.Naive))
                 %My idea:
-                arr = [x_thresh; snr_calc]';
+                arr = [x_thresh; snr_calc]'
                 labels = [labels, string(InputTypes.Naive)];
                 
             end
